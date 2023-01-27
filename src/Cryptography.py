de78e0f8f24e8643b1c7e17125de6c65c9e93db0
Klas-Norling <klasaman99@hotmail.com>
2023-01-27 23:41:53 +0100

    Rewrote some of the classes

--- src/Cryptography.py
+++ src/Cryptography.py
@@ -1,20 +1,24 @@
+from typing import Type
 from rabe_py import aw11
 from rabe_py import ac17
 
 class ABEscheme:
-    public_key = None
-    master_secret_key = None
+    def encrypt(self, plaintext:str) -> None:
+        pass
+
+    def decrypt(self, ciphertext:str) -> str:
+        pass
     
 
 class ABE:
-    def __init__(scheme:ABEscheme, attributes:list[str], policy:str):
-        match scheme:
-            case Aw11:
-                #keygeneration?
-                aw11 = Aw11(attributes, policy)
-            case Ac17:
-                ac17 = Ac17(attributes, policy)
-        pass
+    def __init__(scheme:Type, attributes:list[str], policy:str):
+        if type(scheme) != Type:
+            return "Scheme has to be either ac17 or aw11"
+        if scheme == Aw11:
+            aw11 = Aw11(attributes, policy)
+        if scheme == Ac17:
+            ac17 = Ac17(attributes, policy)
+
 
 
 class Aw11(ABEscheme):
@@ -22,17 +26,22 @@ class Aw11(ABEscheme):
         self.attributes = attributes
         self.policy = policy 
 
-    def encrypt(plaintext:str, attributes:list[str], policy:str):
+    def encrypt(self, plaintext:str):
         gk = aw11.setup()
-        (pk, msk) = aw11.authgen(gk, attributes)
-        ciphertext = aw11.encrypt(gk, pk, policy, plaintext)
+        #Save to target storage
+        (pk, msk) = aw11.authgen(gk, self.attributes)
+        ciphertext = aw11.encrypt(gk, pk, self.policy, plaintext)
         return gk, ciphertext, msk
     
     def decrypt(gk, ciphertext, msk, user_name:str, user_attribute:list[str]):
         sk = aw11.keygen(gk, msk, user_name, user_attribute)
         plaintext = aw11.decrypt(gk, sk, ciphertext)
         return plaintext
-        
+    
+    def keygen(self):
+        gk = aw11.setup()
+        (pk, msk) = aw11.authgen(gk, self.attributes)
+        return gk, pk, msk
         
 
 class Ac17(ABEscheme):
